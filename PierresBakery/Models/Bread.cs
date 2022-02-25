462778a99a54685459373b7c9f7723ccbfba5141
jakechaley <jakechaley@gmail.com>
2022-02-25 13:37:58 -0800

    restructure constructor to include price

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -2,10 +2,12 @@ namespace PierresBakeryBread.Models
 {
   public class Bread 
   {
-    public int UserBread { get; }
+    public int UserBread { get; set; }
+    public int Price { get; }
     public Bread(int breadInput)
     {
       UserBread = breadInput;
+      Price = 5;
     }
 
     public int CalculateCost()
