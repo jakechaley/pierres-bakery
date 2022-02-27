b5281d46ca3d74f34b30b32cdf97f37378e39625
jakechaley <jakechaley@gmail.com>
2022-02-27 13:18:42 -0800

    pass GetPastryInput

--- PierresBakery.Tests/Models/PastryTests.cs
+++ PierresBakery.Tests/Models/PastryTests.cs
@@ -7,7 +7,17 @@ public class PierresBakeryPastryTests
   [TestMethod]
   public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
   {
-    Pastry newPastry = new Pastry();
+    Pastry newPastry = new Pastry(3);
     Assert.AreEqual(typeof(Pastry), newPastry.GetType());
   }
+
+  [TestMethod]
+  public void GetPastryInput_ReturnPastryInput_int()
+  {
+    int userInput = 3;
+    Pastry newPastry = new Pastry(userInput);
+    int result = newPastry.PastryAmount;
+    Assert.AreEqual(userInput, result);
+  }
 }
+
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -2,8 +2,13 @@ namespace PierresBakeryBread.Models
 {
   public class Pastry
   {
-    
+    public int PastryAmount { get; set; }
+    public int Price { get; }
+    public Pastry(int pastryAmount)
+    {
+      PastryAmount = pastryAmount;
+      Price = 2;
+    }
   }
 }
-
 
