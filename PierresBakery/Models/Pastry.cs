97797e31e21fee1f418bea6c0c5a1dda6c2fe23b
jakechaley <jakechaley@gmail.com>
2022-02-27 14:20:56 -0800

    pass CostWithDeal test

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -1,18 +1,25 @@
+using System;
+
 namespace PierresBakeryBread.Models
 {
   public class Pastry
   {
     public int PastryAmount { get; set; }
-    public int Price { get; }
+    public int PriceFor1Pastry { get; }
+    public int PriceFor3Pastry { get; }
     public Pastry(int pastryAmount)
     {
       PastryAmount = pastryAmount;
-      Price = 2;
+      PriceFor1Pastry = 2;
+      PriceFor3Pastry = 5;
     }
 
     public int CalculateCost()
     {
-      return PastryAmount * Price;
+      decimal bundleAmount = Decimal.Floor(PastryAmount / 3);
+      int intBundleAmount = Decimal.ToInt32(bundleAmount);
+      int remainderPastry = PastryAmount % 3; 
+      return (intBundleAmount * PriceFor3Pastry) + (remainderPastry * PriceFor1Pastry);
     }
   }
 }
